0b8ef233162ac47969a6ebdceadfbd39eb3bb815
dxmari <dxmari007@gmail.com>
2024-01-29 15:48:18 +0530

    encodeAndDecodeSpecialCharacters method added

--- multiuse.js
+++ multiuse.js
@@ -1,25 +1,29 @@
-function encodeAndDecodeSpecialCharacters(str = "", type = "en") {  // en for encode & other than en for decode
-  const reverseAsciiValues = { "!": "33", "#": "35", "$": "36", "% ": "37", " & ": "38", "\'": "39", "(": "40", ")": "41", "+": "43", ",": "44", "-": "45", "/": "47", ":": "58", ";": "59", "<": "60", "=": "61", ">": "62", "?": "63", "@": "64", "[": "91", "\\\\": "92", "]": "93", "^": "94", "_": "95", "`": "96", "{": "123", "|": "124", "}": "125", "~": "126" }
+'use-strict';
+var mue = {};
+(function () {
+  mue.encodeAndDecodeSpecialCharacters = function (str = "", type = "en") {  // en for encode & other than en for decode
+    const reverseAsciiValues = { "!": "33", "#": "35", "$": "36", "% ": "37", " & ": "38", "\'": "39", "(": "40", ")": "41", "+": "43", ",": "44", "-": "45", "/": "47", ":": "58", ";": "59", "<": "60", "=": "61", ">": "62", "?": "63", "@": "64", "[": "91", "\\\\": "92", "]": "93", "^": "94", "_": "95", "`": "96", "{": "123", "|": "124", "}": "125", "~": "126" }
 
-  let result = str;
-  let temp;
+    let result = str;
+    let temp;
 
-  if (type === 'en') {
-    for (const itr of str) {
-      if (reverseAsciiValues[itr]) {
-        if (itr === '?') {
-          temp = /\?/g;
-        } else {
-          temp = new RegExp(itr, 'g');
+    if (type === 'en') {
+      for (const itr of str) {
+        if (reverseAsciiValues[itr]) {
+          if (itr === '?') {
+            temp = /\?/g;
+          } else {
+            temp = new RegExp(itr, 'g');
+          }
+          result = result.replace(temp, `___${reverseAsciiValues[itr]}___`);
         }
-        result = result.replace(temp, `___${reverseAsciiValues[itr]}___`);
+      }
+    } else {
+      for (const itr in reverseAsciiValues) {
+        temp = `___${reverseAsciiValues[itr]}___`;
+        result = result.replace(new RegExp(`${temp}`, 'g'), itr);
       }
     }
-  } else {
-    for (const itr in reverseAsciiValues) {
-      temp = `___${reverseAsciiValues[itr]}___`;
-      result = result.replace(new RegExp(`${temp}`, 'g'), itr);
-    }
+    return result;
   }
-  return result;
-}
+})();
